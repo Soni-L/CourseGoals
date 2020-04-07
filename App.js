5fcc51654aeb0a15a309f4502392698aa4517d05
soni lamkaj <acdsoni@gmail.com>
2020-04-07 16:26:03 +0200

    added state change list update on button click and applied styles

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.input} />
-        <Button title="ADD" />
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View></View>
+      <View>
+        {courseGoals.map((goal, index) => (
+          <View key={index} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
@@ -25,5 +47,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     width: "80%",
     padding: 10,
+  },
+  listItem: {
+    padding : 10,
+    backgroundColor : '#ccc',
+    borderColor : 'black',
+    borderWidth : 1
   },
 });
