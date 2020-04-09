c5f395d1a5df5ebf03e646f2283d5611ada00469
soni lamkaj <acdsoni@gmail.com>
2020-04-09 18:31:39 +0200

    split into components

--- App.js
+++ App.js
@@ -9,37 +9,23 @@ import {
   FlatList,
 } from "react-native";
 
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  };
-
-  const addGoalHandler = () => {
-    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+  const addGoalHandler = goal => {
+    setCourseGoals((currentGoals) => [...currentGoals, goal]);
   };
 
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Course Goal"
-          style={styles.input}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        <Button title="ADD" onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler}/>
       <FlatList
         keyExtractor={(item, index) => index.toString()}
         data={courseGoals}
-        renderItem={(itemData) => (
-          <View style={styles.listItem}>
-            <Text>{itemData.item}</Text>
-          </View>
-        )}
+        renderItem={(itemData) => <GoalItem title={itemData.item}/>}
       ></FlatList>
     </View>
   );
@@ -47,22 +33,4 @@ export default function App() {
 
 const styles = StyleSheet.create({
   screen: { padding: 50 },
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  input: {
-    borderColor: "black",
-    borderWidth: 1,
-    width: "80%",
-    padding: 10,
-  },
-  listItem: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: "#ccc",
-    borderColor: "black",
-    borderWidth: 1,
-  },
 });
